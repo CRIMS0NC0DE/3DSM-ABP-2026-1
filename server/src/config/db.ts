5b70e1c308fd0a8588c6032f854f087aca1fa076
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-03-31 19:22:30 -0300

    feat: Inicialização do PrismaORM no projeto.

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,35 +1,19 @@
-import { Pool } from "pg";
+import { PrismaClient } from "@prisma/client";
 
 class Database {
-    private static instance: Database;
-    private dbUrl: string | undefined = process.env.DATABASE_URL;
-    private pool: Pool | null = null;
+  private static instance: PrismaClient;
 
-    // Construtor privado previne criação de novas instâncias fora da classe
-    private constructor() {}
+  private constructor() {}
 
-    // Método público para obter a única instância da conexão
-    public static getInstance(): Database{
-        if(!Database.instance) {
-            Database.instance = new Database();
-        } 
-        return Database.instance
+  public static getInstance(): PrismaClient {
+    if (!Database.instance) {
+      Database.instance = new PrismaClient({
+        log: ["query", "error", "warn"], // Opcional: para ver o que acontece no terminal
+      });
     }
-
-    // Método para iniciar a conexão
-    public async connect(): Promise<Pool> {
-        if (!this.dbUrl) {
-            throw new Error("DATABASE_URL não definido no ambiente.");
-        }
-
-        if (!this.pool) {
-            this.pool = new Pool({ connectionString: this.dbUrl });
-            // Testa a conexão uma vez para falhar rápido se houver problema
-            await this.pool.query("SELECT 1");
-        }
-
-        return this.pool;
-    }
+    return Database.instance;
+  }
 }
 
-export default Database.getInstance();
+// Exportamos a instância pronta para uso
+export const prisma = Database.getInstance();
